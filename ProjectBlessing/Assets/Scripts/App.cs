b7699a5d037d4185e9e0e62a4957bc67650673d9
litsungyi <litsungyi@teamsignalgames.com>
2016-01-30 12:08:02 +0800

    Add App&DeepClone

---
+++ ProjectBlessing/Assets/Scripts/App.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class App : Singleton<App>
+{
+	public GameData Hero;
+
+	public GameData Monster;
+}
---
+++ ProjectBlessing/Assets/TSUtil/TSGeneral/DeepClone.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static partial class ExtensionMethods
+{
+	public static T DeepClone<T>( this T a )
+	{
+		using ( MemoryStream stream = new MemoryStream() )
+		{
+			BinaryFormatter formatter = new BinaryFormatter();
+			formatter.Serialize( stream, a );
+			stream.Position = 0;
+			return ( T ) formatter.Deserialize( stream );
+		}
+	}
+}
