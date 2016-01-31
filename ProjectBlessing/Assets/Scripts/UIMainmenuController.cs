4bce28adac17984ca7de987c4895f2050836e45c
litsungyi <litsungyi@teamsignalgames.com>
2016-01-31 14:55:32 +0800

    Fix Credit button

--- ProjectBlessing/Assets/Scripts/UIMainmenuController.cs
+++ ProjectBlessing/Assets/Scripts/UIMainmenuController.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class UIMainmenuController : MonoBehaviour 
 {
 	public GameObject StartBtn;
+	public GameObject CreditBtn;
 	// Use this for initialization
 	void Start ()
 	{
 		EventTriggerListener.Get(StartBtn).onClick = OnBtnClick;
+		//EventTriggerListener.Get(CreditBtn).onClick = OnCreditBtnClick;
 	}
 	
 	// Update is called once per frame
